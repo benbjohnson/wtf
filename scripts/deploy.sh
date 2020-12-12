36e04e6e5eef8b9c37ff8046750f5f4db366c4b7
Ben Johnson <benbjohnson@yahoo.com>
2020-12-12 08:06:14 -0700

    Add deploy workflow

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,21 +4,12 @@
 rm -rf wtf
 tar zxvf wtfd.tar.gz
 
-# Ensure var/lib directory exists.
-mkdir -p /var/lib/wtfd && chown wtf:wtf /var/lib/wtfd && chmod 755 /var/lib/wtfd
-
-# Copy configuration & credentials.
-mkdir -p /etc/wtfd && chown wtf:wtf /etc/wtfd && chmod 0755 /etc/wtfd
+s# Copy configuration & credentials.
 chown wtf:wtf wtf/wtfd.conf && chmod 0644 wtf/wtfd.conf && cp wtf/wtfd.conf /etc/wtf/
 
 # Set binary permissions.
 chown wtf:wtf wtf/wtfd && chmod 0755 wtf/wtfd
 setcap cap_net_bind_service=+ep wtf/wtfd
-
-# Deploy service.
-chown root:root wtf/wtfd.service && chmod 0644 wtf/wtfd.service
-cp wtf/wtfd.service /etc/systemd/system/
-systemctl enable wtfd.service
 
 # Restart service.
 service wtfd stop
--- scripts/provision.sh
+++ scripts/provision.sh
@@ -26,6 +26,13 @@ chown benbjohnson:benbjohnson /home/benbjohnson/.ssh && chmod 700 /home/benbjohn
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCpezYgoL33WgiS73KUHyvcsW5UZwp2SKV65QH5bdgZKxUDTSJKkzSSI+qPcueg8FLKYtKdZ2HBn+uYXzHabqRpmtw66Us2bJY0iitzG3V1Szb2RXZNPaf/eKWULUx55XaKZCVg8/viifAMGeC+SI6BdUQFD3LXpzGkRWWfKoqpZGD92400ORcmhOBW/a3afA34L3y+Z6O8LQ57RAHQxwKeTyWc/rgHoLvlyx3H3bV4kGAITCHJDSX+OSvyrwsx9yoi2CHiYBCt5K+gZJMV2P99uro4pyQF+6baDUtQJOW7sT2+2HTHNV8xLyutMDY6w8BQZdj/Z5+vRF0RUdBPi10q8xgb0N3fGkdF+c5W83K9+WwjPtQiNAwIGwrXzaQYsEm6ejkrkJQ6ZilX+cvDya0n42XxCooPRxNxCd9Wqq1o3aTR4ophFA8O1Dnei8RJo3mVuuVa7d8tXiVuTH/cYRYWsty8R/ueJ6Ipgwng4WnChG28beIhP3h9xMz4wi7BBRcazIk9nT/o5o/rcAd7TW+XHmyMdg75zKFBlEAKYWpbkyKesTaE5Ck1gYRmPIiXCcrzl7fg5+q25xFSHXFG4G/v3IpRFgK7AAOVwvJdsPP9m79Zj1g8FdZ/Tzr3yijy+++Y/Zh96O+z0+slkt77aj/NHGoa28jwgyP+oD4Qv7WMww== benbjohnson@yahoo.com" > /home/benbjohnson/.ssh/authorized_keys
 chown benbjohnson:benbjohnson /home/benbjohnson/.ssh/authorized_keys && chmod 600 /home/benbjohnson/.ssh/authorized_keys
 
+# Create deploy user with login key.
+useradd -m -G wheel -s /bin/bash deploy
+mkdir -p /home/deploy/.ssh
+chown deploy:deploy /home/deploy/.ssh && chmod 700 /home/deploy/.ssh
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQD0X463ba3Ac5XToVW100A2KeQRJ2pkX75TPxv1weXrTpfZqeAvL5bG9Dn5w5M2ujsp7ATLoSIXxB5e1dk0IzJ93e+3F3XJ1Qw4okV7sqMiim4O19u73CM4LKRj4V5DH0ieqnybVBg3uS8V+R5658vPSYPk2MpQQUTZrLI4YGsg89KH3UUQgQQpJSdCLvgR4yZ/W8knvlV07sgKbGlWDsw5az/aYzknIukRBHGo4oHr6OK5Cm4ozQ6iprkLTM9zuZYRiCDzctq49HM3gYHZAcwQtBVSu+Hv9aH5DDYNfngoNeRTS9exLzMmhrMMPIDO+7hFmaD58p4jnYvC6QzadLUnCM5zPQAtrQ1NusmibcjHkHhpom0bl/8TpmvXgASJN7NRkFctdzaL0By4uRPBqWpiCNE2YxT7gTlmluet7Vk48hCn6RxRWc0EMP4nGXrdvtAxsaHjYrWGoFstZWPeMOF65PdF20TFJoorv4ApsHrvvGPvvvyzyFg9G1Abi5oss/l7vaFbdSPzvjv3VJcGinuuy+bgpwT8M+lkGQfsGgDNrQu81V2T/OangJ8ML3sKrt6yj7qEJ58gOguRzshjD8tnlXOx70V5XMCxhTpXM5yrhPCYtoMnR/04ra5jwNVo/cOXo7wCbNqr05SHqi7PU+PS3NTXdPjSjU6qph9kFSP55Q== deploy@gobeyond.dev" > /home/deploy/.ssh/authorized_keys
+chown deploy:deploy /home/deploy/.ssh/authorized_keys && chmod 600 /home/deploy/.ssh/authorized_keys
+
 # Install and update packages.
 apt update && apt upgrade -y
 apt install -y ufw unattended-upgrades
@@ -40,6 +47,29 @@ EOF
 
 # Create user.
 useradd -s /sbin/nologin wtf
+
+# Install systemd service.
+cat <<EOF > /etc/systemd/system/wtfd.service
+[Unit]
+Description=WTF Dial
+
+[Service]
+User=wtf
+Group=wtf
+Restart=on-failure
+ExecStart=/usr/local/bin/wtfd -config /etc/wtfd/wtfd.conf
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+chown root:root /etc/systemd/system/wtfd.service
+chmod 0644 /etc/systemd/system/wtfd.service
+systemctl enable wtfd.service
+
+# Configure data & configuration directories.
+mkdir -p /var/lib/wtfd && chown wtf:wtf /var/lib/wtfd && chmod 755 /var/lib/wtfd
+mkdir -p /etc/wtfd && chown wtf:wtf /etc/wtfd && chmod 0755 /etc/wtfd
 
 # Configure & enable firewall.
 ufw default deny incoming
